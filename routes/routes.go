d80bbcf71ed6d6c8d861954575e2b099f46dede2
Lucca <luccajbecker@gmail.com>
2025-03-11 16:47:30 -0300

    feat: defines initial routes

--- routes/routes.go
+++ routes/routes.go
@@ -8,13 +8,22 @@ import (
 
 func initializeRoutes(router *gin.Engine) {
 
-	// endpoint
+	// Endpoint
 	end := router.Group("/api/reef-metrics/v1")
 
-	// test
-	end.GET("/ping", func(ctx *gin.Context) {
+	// Route to get all data from event_key
+	end.GET("/event/:event_key", func(ctx *gin.Context) {
+		event_key := ctx.Param("event_key")
 		ctx.JSON(http.StatusOK, gin.H {
-			"message": "pong",
+			"test": event_key,
+		})
+	})
+
+	// Route to get the sum of each row from event_key
+	end.GET("/event/:event_key/simple", func(ctx *gin.Context) {
+		event_key := ctx.Param("event_key")
+		ctx.JSON(http.StatusOK, gin.H {
+			"test": event_key,
 		})
 	})
 }
