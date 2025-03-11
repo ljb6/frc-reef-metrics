decd8982c641d1f8a59fcca67096c84f9674dd67
Lucca <luccajbecker@gmail.com>
2025-03-11 16:17:02 -0300

    feat: router setup

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,7 @@ func initializeRoutes(router *gin.Engine) {
 	// endpoint
 	end := router.Group("/api/reef-metrics/v1")
 
+	// test
 	end.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H {
 			"message": "pong",
