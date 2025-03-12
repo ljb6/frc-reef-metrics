d88a83e6ab99a2842d36c016d1a68551fd260610
Lucca <luccajbecker@gmail.com>
2025-03-12 17:12:37 -0300

    feat: func to open the db

--- db/conn.go
+++ db/conn.go
@@ -1,3 +1,27 @@
 package db
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	postgresSqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", postgresSqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to DB: " + dbname)
+
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	router "github.com/ljb6/frc-reef-metrics/routes"
-)
-
 func main() {
+
+	// dbConnection, err := db.ConnectDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	router.InitializeServer()
 }
