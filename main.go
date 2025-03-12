4f06167c70e72ecadcc4cc57eaf29c19f0160faa
Lucca <luccajbecker@gmail.com>
2025-03-12 10:29:36 -0300

    refactor: back to initial setup

--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	database "github.com/ljb6/frc-reef-metrics/db"
-	"github.com/ljb6/frc-reef-metrics/models"
 	router "github.com/ljb6/frc-reef-metrics/routes"
 )
 
 func main() {
-	database.OpenDatabase()
-	defer database.CloseDatabase()
-
-	database.Migrate()
-
-	test_data := models.EventStats{
-		Event_key: "brba2024",
-		Week: "Week 3",
-		Auto_data: `"botRow": { "nodeA": 3, "nodeB": 5, "nodeC": 2 },
-					"midRow": { "nodeA": 4, "nodeB": 1, "nodeC": 3 },
-					"topRow": { "nodeA": 2, "nodeB": 3, "nodeC": 1 },
-					"trough": 0`,
-		Tele_data: `"botRow": { "nodeA": 3, "nodeB": 5, "nodeC": 2 },
-					"midRow": { "nodeA": 4, "nodeB": 1, "nodeC": 3 },
-					"topRow": { "nodeA": 2, "nodeB": 3, "nodeC": 1 },
-					"trough": 0`,
-	}
-
-	database.InsertData(test_data)
-
 	router.InitializeServer()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,19 +9,10 @@ func initializeRoutes(router *gin.Engine) {
 	// Endpoint
 	end := router.Group("/api/reef-metrics/v1")
 
-	// Route to get all data from event_key
-	end.GET("/event/:event_key", func(ctx *gin.Context) {
-		event_key := ctx.Param("event_key")
-		ctx.JSON(http.StatusOK, gin.H {
-			"test": event_key,
-		})
-	})
-
-	// Route to get the sum of each row from event_key
-	end.GET("/event/:event_key/simple", func(ctx *gin.Context) {
-		event_key := ctx.Param("event_key")
-		ctx.JSON(http.StatusOK, gin.H {
-			"test": event_key,
+	// Test
+	end.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H {
+			"message": "pong",
 		})
 	})
 }
